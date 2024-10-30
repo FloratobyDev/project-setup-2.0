42e0715d479ffa47c3a4f96866d4c18343d32b70
Michael Mushrush <mushrushm@gmail.com>
2024-10-30 02:24:55 -0400

    new changes

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,13 +1,14 @@
-const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+const API_URL = "https://app.affordabletesturls.online";
 
 export const fetchData = async () => {
+  console.log("API_URL:", API_URL);
+
   try {
     const response = await fetch(`${API_URL}/api/data`);
-    if (!response.ok) throw new Error('Network response was not ok');
+    if (!response.ok) throw new Error("Network response was not ok");
     return await response.json();
   } catch (error) {
-    console.error('Fetch data error:', error);
+    console.error("Fetch data error:", error);
     return null;
   }
 };
-
